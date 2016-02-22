4f904228e414e07f52d7040a08da39966af7ea90
j9794 <juanmanuel9794@hotmail.com>
2016-02-22 19:53:57 -0300

    Beta Reduction and tests.

--- src/main/scala/calculoLambda/CalculoLambda.scala
+++ src/main/scala/calculoLambda/CalculoLambda.scala
@@ -4,9 +4,30 @@ import scala.util.parsing.combinator._
 
 object CalculoLambda {
   trait Expr
+  {
+    def replace(toReplace:Expr, replacement:Expr):Expr
+    def reduce:Expr
+    def apply(param:Expr):Expr
+  }
   case class Lambda(par:Var, body: Expr) extends Expr
+  {
+    def reduce = Lambda(par.reduce,body.reduce)
+    def apply(param:Expr) = body.replace(par,param)
+    def replace(toReplace:Expr,replacement:Expr) = body.replace(toReplace,replacement)  
+  }
   case class Var(id:String) extends Expr
+  {
+    def reduce = this
+    def apply(param:Expr) = App(this,param)
+    def replace(toReplace:Expr,replacement:Expr) = if (toReplace == this) replacement else this
+  }
   case class App(par1:Expr, par2:Expr) extends Expr
+  {
+    def reduce = par1.reduce.apply(par2.reduce)
+    def apply(param:Expr) = this
+    def replace(toReplace:Expr,replacement:Expr) =
+      App(par1.replace(toReplace,replacement),par2.replace(toReplace,replacement))
+  }
   
   trait LambdaParser extends RegexParsers{
     
---
+++ src/test/scala/calculoLambda_test/ReduceCalculoLambdaTest.scala
@@ -0,0 +1,58 @@
+package calculoLambda_test
+
+import org.scalatest._
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import calculoLambda.CalculoLambda._
+
+class ReduceCalculoLambdaTest extends FlatSpec with Matchers with LambdaParser {
+
+  val vA = Var("A")
+  val vB = Var("B")
+  val appAB = App(vA,vB)
+  val lambdaIdentity = Lambda(vA,vA)
+  val lambdaConstant:Var=>Lambda = v => if(v == vA) Lambda(vB,v) else Lambda(vA,v) 
+  
+  "A var" should "return itself" in {
+
+    vA.reduce shouldBe vA
+
+  }
+  
+  "An application of two vars" should "return itself" in {
+
+    appAB.reduce shouldBe appAB
+
+  }
+
+  "A lambda of varA as parameter and varB as body" should "return itself" in {
+
+    Lambda(vA,vB).reduce shouldBe Lambda(vA,vB)
+
+  }
+
+  "A lambda with a var and a reducible app" should "return a lambda with the reduced app" in {
+
+    Lambda(vB,App(lambdaIdentity,vB)).reduce shouldBe Lambda(vB,vB)
+
+  }
+    
+  "An application of a lambda and a var" should "return the result of the lambda" in {
+
+    App(lambdaIdentity,vB).reduce shouldBe vB
+
+  }
+  
+  "An app of a var and an app of a lambda and a var" should "return the app of the reduced app and the var" in {
+
+    App(App(lambdaIdentity,vB),vA).reduce shouldBe App(vB,vA)
+
+  }
+  
+  "Complex expression" should "return the same as in paper" in { //Aca llegue a mi limite para nombres expresivos en los tests
+
+    App(App(Lambda(Var("X"),Lambda(Var("Y"),Var("X"))),Lambda(Var("Z"),Var("Z"))),Lambda(Var("A"),Var("A"))).reduce shouldBe Lambda(Var("A"),Var("A"))
+
+  }
+     
+}
